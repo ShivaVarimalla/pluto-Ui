afa1a411d12f563de74e5f043aa5f820ba20f24b
shivashankar1253 <varimallashankar@gmail.com>
2020-11-19 16:31:54 +0530

    changes commited

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Footer.css'
+import TrendingFlatIcon from '@material-ui/icons/TrendingFlat';
 
 function Footer (){
     return (
@@ -8,47 +9,48 @@ function Footer (){
                 <div className=" content1">
                     <h3 className="head">Top Products</h3>
                         <ul className="Ul">
-                            <li>Managed Website</li>
-                            <li>Manage Reputation</li>
-                            <li>Power Tools</li>
-                            <li>Marketing Services</li>
+                            <li className="list-element">Managed Website</li>
+                            <li className="list-element">Manage Reputation</li>
+                            <li className="list-element">Power Tools</li>
+                            <li className="list-element">Marketing Services</li>
                         </ul>
                 </div>
                 <div className="content2">
                     <h3  className="head">Quick Links</h3>
                         <ul className="Ul">
-                            <li>Jobs</li>
-                            <li>Brand Assets</li>
-                            <li>Investor Realtions</li>
-                            <li>Trems of Service</li>
+                            <li className="list-element">Jobs</li>
+                            <li className="list-element">Brand Assets</li>
+                            <li className="list-element">Investor Realtions</li>
+                            <li className="list-element">Trems of Service</li>
                         </ul>
                 </div>
                 <div className="content3">
                     <h3  className="head">Features</h3>
                         <ul className="Ul">
-                            <li>Jobs</li>
-                            <li>Brand Assets</li>
-                            <li>Investor Realtions</li>
-                            <li>Trems of Service</li>
+                            <li className="list-element">Jobs</li>
+                            <li className="list-element">Brand Assets</li>
+                            <li className="list-element">Investor Realtions</li>
+                            <li className="list-element">Trems of Service</li>
                         </ul>
                 </div>
-                <div className="content3">
+                <div className="content4">
                     <h3  className="head">Resoures</h3>
                         <ul className="Ul">
-                            <li>Guides</li>
-                            <li>Research</li>
-                            <li>Expercts</li>
-                            <li>Agencies</li>
+                            <li className="list-element">Guides</li>
+                            <li className="list-element">Research</li>
+                            <li className="list-element">Expercts</li>
+                            <li className="list-element">Agencies</li>
                         </ul>
                 </div>
-                <div className="content3">
+                <div className="content5">
                     <h3  className="head">NewsLetter</h3>
                         <ul className="Ul">
-                            <li>Stay Update with our latest</li>
+                            <li className="list-element">Stay Update with our latest</li>
                         </ul>
-                        <input className="input" placeholder="Enter Email Address"/><button className="Button">Enter</button>
+                       <div classname="containerInput"> <input className="input" placeholder="Enter Email Address"/><div className="Button"><TrendingFlatIcon classname="icon"/></div></div>
                 </div>
             </div>
+            <p className="text">Copyright ©2020 All rights reserved</p>
         </div>
     )
 }
